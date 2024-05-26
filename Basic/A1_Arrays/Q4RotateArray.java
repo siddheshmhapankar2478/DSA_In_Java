4fa59a1d157bb1e02159094a449eac79e35c91a5
Siddhesh <sidd.mhapankar7@gmail.com>
2024-05-26 23:22:54 +0530

    solved questions related to arrays and basic 2d arrays question

--- Basic/A1_Arrays/Q4RotateArray.java
+++ Basic/A1_Arrays/Q4RotateArray.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class Q4RotateArray {
     public static void main(String[] args) {
-        int k=3;
+        int k=2;
         int[] arr={10,20,30,40,50,60,70};
         reverseArrByRight(arr,k);
+        reverseArrByLeft(arr,k);
     }
     public static void reverse(int[] arr,int start,int end){
         while(start<end){
@@ -23,6 +24,15 @@ public class Q4RotateArray {
         reverse(arr, 0, n-1);
         reverse(arr, 0, k-1);
         reverse(arr, k, n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+    public static void reverseArrByLeft(int[] arr,int k){
+        int n=arr.length;
+        k=k%n;
+        reverse(arr, 0, n-1);
+        reverse(arr, 0, n-k-1);
+        reverse(arr, n-k, n-1);
         System.out.println(Arrays.toString(arr));
     }
     
